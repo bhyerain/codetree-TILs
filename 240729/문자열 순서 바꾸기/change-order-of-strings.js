23b92a0af70f1a9fb52b5a47b9dca7bbe75b139d
Seunghye Yang <155219213+bhyerain@users.noreply.github.com>
2024-07-29 15:48:22 +0900

    Title: 문자열 순서 바꾸기, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

--- 240729/문자열 순서 바꾸기/change-order-of-strings.js
+++ 240729/문자열 순서 바꾸기/change-order-of-strings.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 
 let input = fs.readFileSync(0).toString().trim().split("\n");
-let s = input[0]
-let t = input [1]
-console.log(`${t}
-${s}`);
+let s = input[0];
+let t = input [1];
+
+[s, t] = [t, s];
+console.log(s);
+
+console.log(t);
