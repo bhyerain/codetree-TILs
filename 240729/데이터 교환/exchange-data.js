67e759c2a6c21bf07ab52843892bb3592ebb692c
Seunghye Yang <155219213+bhyerain@users.noreply.github.com>
2024-07-29 12:39:40 +0900

    Title: 데이터 교환, Time: 166ms, Memory: 15MB, Status: Passed - CodeTree

--- 240729/데이터 교환/exchange-data.js
+++ 240729/데이터 교환/exchange-data.js
@@ -2,8 +2,8 @@ let a = 5;
 let b = 6;
 let c = 7;
 
-[a, b, c] = [c, a, b]
+[a, b, c] = [c, a, b];
 
-console.log(a)
-console.log(b)
-console.log(c)
+console.log(a);
+console.log(b);
+console.log(c);
