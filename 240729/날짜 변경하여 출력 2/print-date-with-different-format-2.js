7bdb4067f9ce92886e48db5411a45062254f39e4
Seunghye Yang <155219213+bhyerain@users.noreply.github.com>
2024-07-29 15:54:51 +0900

    Title: 날짜 변경하여 출력 2, Time: 199ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240729/날짜 변경하여 출력 2/print-date-with-different-format-2.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim().split("-");
+
+let year = Number(input[2]);
+let month = Number(input[0]);
+let day = Number(input[1]);
+
+console.log(`${year}.${month}.${day}`);
